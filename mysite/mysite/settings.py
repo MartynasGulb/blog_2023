d6c684c421ad69f3f3b5d6df305310a544a1e0c7
Martynas Gulbinavicius <martynass98@gmail.com>
2023-06-08 14:51:09 +0300

    2023.06.08

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,15 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 from .my_settings import (SECRET_KEY, EMAIL_HOST_USER,
                           EMAIL_HOST_PASSWORD, DEBUG,
-                          ALLOWED_HOSTS, STATICFILES_DIRS,
-                          STATIC_ROOT)
+                          ALLOWED_HOSTS, #STATICFILES_DIRS,
+                          STATIC_ROOT, BASE_DIR, STATIC_URL)
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -152,8 +151,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = STATIC_ROOT
-STATICFILES_DIRS = STATICFILES_DIRS
-STATIC_URL = '/static/'
+# STATICFILES_DIRS = STATICFILES_DIRS
+STATIC_URL = STATIC_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
